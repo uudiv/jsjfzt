a494fee2c7efe36caf7e9d4ed4b2da8ba44251b1
zxz <690792862@qq.com>
2019-02-03 11:01:05 +0800

    完成

--- js/index.js
+++ js/index.js
@@ -1,27 +1,12 @@
 $(function () {
-	$('.lbt_news .top ul li').on('mouseover', function () {
+	$('.tpxw .top ul li').on('mouseover', function () {
 		$(this).addClass("now").siblings().removeClass("now");
-		$('.lbt_news .top > a').attr('href', $(this).children().attr('href'))
-		$('.lbt_news .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
+		$('.tpxw .top > a').attr('href', $(this).children().attr('href'))
+		$('.tpxw .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
 	});
-	lbt_wz(".lbt .img li", ".lbt .img", ".lbt .num", ".lbt .num li", ".lbt", "now", 617, ".lbt .hwz > div");
-	jQuery(".picScroll-left").slide({
-		titCell: ".hd ul",
-		mainCell: ".bd ul",
-		autoPage: true,
-		effect: "left",
-		autoPlay: true,
-		scroll: 5,
-		vis: 5,
-		delayTime: 1000
-	});
-	$('.box3 .left ul li').on('click',function(){
+	$('.zxwj .top ul li').on('mouseover', function () {
 		$(this).addClass("now").siblings().removeClass("now");
-		$('.box3 .right > div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
-	})
-	$('.zcjd .top ul li').on('mouseover', function () {
-		$(this).addClass("now").siblings().removeClass("now");
-		$('.zcjd .top > a').attr('href', $(this).children().attr('href'))
-		$('.zcjd .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
+		$('.zxwj .top > a').attr('href', $(this).children().attr('href'))
+		$('.zxwj .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
 	});
 })
